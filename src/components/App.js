75324bd0a8ec5e96085da188a056c204af913bce
SoniyaLadhani <ladhanisoniya@johndeere.com>
2020-03-29 17:16:36 +0530

    Initilised boilerplate with webpack

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>My React App p</h1>
+            </div>
+        )
+    }
+}
